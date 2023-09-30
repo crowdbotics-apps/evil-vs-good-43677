bb93f515e0cec014cfeada3619402515f2691b94
Crowdbotics <team@crowdbotics.com>
2023-09-30 19:54:27 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "evil-vs-good-43677",
+  url: "https://evil-vs-good-43677.botics.co",
+  api: "https://evil-vs-good-43677.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
